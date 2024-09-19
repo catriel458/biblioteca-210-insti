7dd285a5d58e9da39062c1145cf6dde95cd6825f
Catriel Cabrera <catrielcabrera97@gmail.com>
2024-09-19 20:58:58 -0300

    Migracion base de datos

--- biblioteca_digital/libros/admin.py
+++ biblioteca_digital/libros/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Inventario  # Importa tu modelo
+from .models import Libro
+from .models import Mapas
+from .models import Multimedia
+from .models import Notebook
+from .models import Proyector
+from .models import Varios
 
-# Register your models here.
+admin.site.register(Inventario)
+admin.site.register(Libro)
+admin.site.register(Mapas)
+admin.site.register(Multimedia)
+admin.site.register(Notebook)
+admin.site.register(Proyector)
+admin.site.register(Varios)
