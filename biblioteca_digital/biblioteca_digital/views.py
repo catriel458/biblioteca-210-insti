b2f518184d31d7bdeddd56403e2cfe44ae95984e
Yaidny <yun.briceno@gmail.com>
2025-04-11 19:17:09 -0300

    chore: actualizar gitignore para excluir archivos de caché y compilados

--- biblioteca_digital/biblioteca_digital/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'home.html', {'header_class': 'header-home'})
-
-def alta_material(request):
-    return render(request, 'materiales/alta_material.html', {'header_class': 'header-pantalla'})
